db0577b5f466bfc42600f6d9a0efa4582506e839
alexhan <alexhan0324@gmail.com>
2020-02-28 07:34:20 -0800

    frontend basic setup -unedited

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar.component";
+import ParamList from "./components/param-list.component";
+import EditParam from "./components/edit-param.component";
+import CreateParam from "./components/create-param.component";
+
+//import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ParamList} />
+      <Route path="/edit/:id" component={EditParam} />
+      <Route path="/create" component={CreateParam} />        
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/create-param.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateParam extends Component {
+    render() {
+        return (
+            <div>
+                <p>You are on the Create Param</p>
+            </div>
+        )
+    }
+}

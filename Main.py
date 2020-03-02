fc3d97d8eec6f5109be40af5afaae868fbd0277d
jjrryyaa <jjrryyaa@gmail.com>
2020-03-02 15:03:24 -0800

    Adding git ignore

--- Main.py
+++ Main.py
@@ -4,7 +4,6 @@ from __future__ import (absolute_import, division, print_function,
 import datetime  # For datetime objects
 import os.path  # To manage paths
 import sys  # To find out the script name (in argv[0])
-import config
 
 # Import the backtrader platform
 import backtrader as bt
